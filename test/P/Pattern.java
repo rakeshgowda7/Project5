2560fba393fecf9cb39b872fab143feeb6152875
rakeshgowda7 <as.rakeshgowda@gmail.com>
2022-11-30 17:22:11 +0530

    Programs added

--- test/P/Pattern.java
+++ test/P/Pattern.java
@@ -4,14 +4,29 @@ public class Pattern {
 
 	public static void main(String[] args) {
 
+		// Increasing order
+
+//		*
+//		**
+//		***
+//		****
+//		*****
+//		******
+
 		for (int i = 0; i <= 5; i++) {
 			for (int j = 0; j <= i; j++) {
 				System.out.print("*");
 			}
 			System.out.println();
 		}
+		// dec order
+//		*****
+//		****
+//		***
+//		**
+//		*
 
-		for (int i = 0; i <= 4; i++) {
+		for (int i = 0; i <= 5; i++) {
 			for (int j = 4; j >= i; j--) {
 				System.out.print("*");
 			}
